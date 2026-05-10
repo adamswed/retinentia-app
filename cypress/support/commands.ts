fdb4b2fd1e5c22f39e622e87b410336b8a3cb828
Adam Swed <adamswed@gmail.com>
2026-05-10 18:22:30 -0400

    Add cypress auth helper command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,6 +2,22 @@ export {};
 
 declare global {
   namespace Cypress {
-    interface Chainable {}
+    interface Chainable {
+      login(): Chainable<void>;
+    }
   }
 }
+
+Cypress.Commands.add('login', () => {
+  cy.session(
+    Cypress.env('TEST_EMAIL'),
+    () => {
+      cy.visit('/email-sign-in');
+      cy.get('#email').type(Cypress.env('TEST_EMAIL'));
+      cy.get('#password').type(Cypress.env('TEST_PASSWORD'), { log: false });
+      cy.get('button[type="submit"]').click();
+      cy.url().should('include', '/main');
+    },
+    { cacheAcrossSpecs: true }
+  );
+});
