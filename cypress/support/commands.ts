0d86249e9dc94bae34c70d79a6a493d983d896e2
Adam Swed <adamswed@gmail.com>
2026-05-10 18:50:58 -0400

    Add cypress login test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -12,12 +12,19 @@ Cypress.Commands.add('login', () => {
   cy.session(
     Cypress.env('TEST_EMAIL'),
     () => {
+      cy.intercept('POST', '/api/consent').as('consent');
       cy.visit('/email-sign-in');
       cy.get('#email').type(Cypress.env('TEST_EMAIL'));
       cy.get('#password').type(Cypress.env('TEST_PASSWORD'), { log: false });
       cy.get('button[type="submit"]').click();
+      cy.wait('@consent');
       cy.url().should('include', '/main');
     },
-    { cacheAcrossSpecs: true }
+    {
+      cacheAcrossSpecs: true,
+      validate() {
+        cy.request({ url: '/main', failOnStatusCode: false }).its('status').should('eq', 200);
+      },
+    }
   );
 });
